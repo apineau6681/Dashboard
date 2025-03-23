5a5d15ba382c27a8fcd4802e4ef8c4995b9968e9
apineau6681 <pineau.antoine@gmail.com>
2025-03-23 11:59:00 +0100

    Update get_google_trends_nb_days.py

--- scripts/get_google_trends_nb_days.py
+++ scripts/get_google_trends_nb_days.py
@@ -28,23 +28,30 @@ start_date = end_date - timedelta(days=num_days)
 trend_start_date = start_date - timedelta(days=1)
 trend_end_date = end_date - timedelta(days=1)
 
+# Open spreadsheet
+output_spreadsheet = client.open_by_key(SPREADSHEET_ID)
+
+# Clear existing data in btc_price and btc_trend sheets
+btc_price_worksheet = output_spreadsheet.worksheet(BTC_PRICE_SHEET)
+btc_trend_worksheet = output_spreadsheet.worksheet(BTC_TREND_SHEET)
+btc_price_worksheet.clear()
+btc_trend_worksheet.clear()
+print("Cleared btc_price and btc_trend sheets.")
+
 # Step 1: Fetch BTC price and store in 'btc_price'
 btc_data = yf.download('BTC-USD', start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
 btc_data = btc_data[['Close']].reset_index()
 btc_data.rename(columns={'Date': 'date', 'Close': 'btc_price'}, inplace=True)
-output_spreadsheet = client.open_by_key(SPREADSHEET_ID)
-btc_price_worksheet = output_spreadsheet.worksheet(BTC_PRICE_SHEET)
 set_with_dataframe(btc_price_worksheet, btc_data)
 print("BTC price data inserted into btc_price tab.")
 
-# Step 2: Fetch Google Trends data and store in 'btc_trend'
+# Step 2: Fetch Google Trends data for Bitcoin and Crypto
 pytrends = TrendReq(hl='en-US', tz=360)
-pytrends.build_payload(['Bitcoin'], timeframe=f'{trend_start_date.strftime("%Y-%m-%d")} {trend_end_date.strftime("%Y-%m-%d")}', geo='', gprop='')
+pytrends.build_payload(['Bitcoin', 'Crypto'], timeframe=f'{trend_start_date.strftime("%Y-%m-%d")} {trend_end_date.strftime("%Y-%m-%d")}', geo='', gprop='')
 trends_df = pytrends.interest_over_time().drop(columns=['isPartial'], errors='ignore').reset_index()
-trends_df.rename(columns={'date': 'date', 'Bitcoin': 'BTC_trend'}, inplace=True)
-btc_trend_worksheet = output_spreadsheet.worksheet(BTC_TREND_SHEET)
+trends_df.rename(columns={'date': 'date', 'Bitcoin': 'BTC_trend', 'Crypto': 'Crypto_trend'}, inplace=True)
 set_with_dataframe(btc_trend_worksheet, trends_df)
-print("BTC trend data inserted into btc_trend tab.")
+print("BTC and Crypto trend data inserted into btc_trend tab.")
 
 # Step 3: Merge data from btc_price and btc_trend, then store in 'Sheet1'
 btc_price_df = pd.DataFrame(btc_price_worksheet.get_all_records())
